14124bad5018ad4f40922c15f4b36d3be471d13a
tmo <34272348+tmo@users.noreply.github.com>
2024-07-23 16:32:35 +1000

    Modified ground truth output format

--- mod_mtd_notify.sh
+++ mod_mtd_notify.sh
@@ -4,7 +4,7 @@ HOOK_FILE=/etc/bind/zones/forward.mj.uq.dslab.com.db
 
 while inotifywait -qq  $HOOK_FILE
 do
-        echo "IP changed, at $(date +'%a %d %b %Y %T.%5N %Z')";
+        echo "IP changed, at $(date +'%a %d %b %Y %T.%5N %Z')" | tr '\n' ',';
         cat $HOOK_FILE | grep "www"
         sudo rndc reload mj.uq.dslab.com
 done
